769a06ce7cbf3bfbb731e03b689b2cbb64dbaf58
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-05 19:24:52 -0500

    Código completo mas ainda não comentado.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
 var slider;
 var angle = 0;
+var len;
+
+
 function setup(){
-   createCanvas(640, 480);
+   createCanvas(400, 400);
    slider = createSlider(0, TWO_PI, PI/4, 0.01);
 }
 
 function draw(){
    background(51);
    angle = slider.value();
-   strocke(255);
+   stroke(255);
    translate(200, height);
    branch(100);
 }
@@ -16,7 +19,7 @@ function draw(){
 function branch(len){
    line(0, 0, 0, -len);
    translate(0, -len);
-      if {(len > 4) { push(); branch(len*0.67)
+      if (len > 4) { push(); branch(len*0.67);
    rotate(angle);
    branch(len*0.67);
    pop();
